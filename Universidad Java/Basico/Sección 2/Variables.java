dba411413929990c41aeac43f84571cb10f5a14e
Dario Usiel Marmolejo Lopez <45133810+DarioMarmolejo@users.noreply.github.com>
2022-08-04 19:14:44 -0500

    Add files via upload

---
+++ Universidad Java/Basico/Sección 2/Variables.java
@@ -0,0 +1,10 @@
+
+public class Variables {
+    
+    public static void main (String[]args){
+
+        int miVariableEntera = 10;
+        System.out.println("variable entera: " + miVariableEntera);
+        
+    }
+}
