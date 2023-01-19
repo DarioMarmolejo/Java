db120f3ec32411858bf355555f7a4d2ded0bd3e4
DarioMarmolejo <xxdausmalo@gmail.com>
2023-01-19 11:53:24 -0600

    UPDATE - Se implemento el import de JobConfig

--- Spring Batch/SpringBatch-HelloWorld/src/main/java/com/example/SpringBatchHelloWorld/SpringBatchHelloWorldApplication.java
+++ Spring Batch/SpringBatch-HelloWorld/src/main/java/com/example/SpringBatchHelloWorld/SpringBatchHelloWorldApplication.java
@@ -2,8 +2,12 @@ package com.example.SpringBatchHelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.example.SpringBatchHelloWorld.configuration.JobConfiguration;
 
 @SpringBootApplication
+@Import(JobConfiguration.class)
 public class SpringBatchHelloWorldApplication {
 
 	public static void main(String[] args) {
